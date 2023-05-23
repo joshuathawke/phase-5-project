d80dd6333bce4ed5c5a4c639afcaed36301d8bab
joshuathawke <joshuathawke@gmail.com>
2023-05-23 13:54:40 -0400

    updates to routes

--- server/app.py
+++ server/app.py
@@ -58,15 +58,132 @@ class Teams(Resource):
         teams = Team.query.all()
         return [team.to_dict() for team in teams]
 
+    def post(self):
+        try:
+            data = request.get_json()
+            team = Team(name=data['name'])
+            db.session.add(team)
+            db.session.commit()
+            return {'message': 'Team created!', 'team': team.to_dict()}, 201
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
+    def put(self, team_id):
+        try:
+            team = Team.query.get(team_id)
+            if not team:
+                return {'message': 'Team not found!'}, 404
+            
+            data = request.get_json()
+            team.name = data['name']
+            db.session.commit()
+            
+            return {'message': 'Team updated!', 'team': team.to_dict()}, 200
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
+    def delete(self, team_id):
+        try:
+            team = Team.query.get(team_id)
+            if not team:
+                return {'message': 'Team not found!'}, 404
+            
+            db.session.delete(team)
+            db.session.commit()
+            
+            return {'message': 'Team deleted!'}, 200
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
 class Tournaments(Resource):
     def get(self):
         tournaments = Tournament.query.all()
         return [tournament.to_dict() for tournament in tournaments]
+
+    def post(self):
+        try:
+            data = request.get_json()
+            tournament = Tournament(name=data['name'], date=data['date'], location=data['location'])
+            db.session.add(tournament)
+            db.session.commit()
+            return {'message': 'Tournament created!', 'tournament': tournament.to_dict()}, 201
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
+    def put(self, tournament_id):
+        try:
+            tournament = Tournament.query.get(tournament_id)
+            if not tournament:
+                return {'message': 'Tournament not found!'}, 404
+            
+            data = request.get_json()
+            tournament.name = data['name']
+            tournament.date = data['date']
+            tournament.location = data['location']
+            db.session.commit()
+            
+            return {'message': 'Tournament updated!', 'tournament': tournament.to_dict()}, 200
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
+    def delete(self, tournament_id):
+        try:
+            tournament = Tournament.query.get(tournament_id)
+            if not tournament:
+                return {'message': 'Tournament not found!'}, 404
+            
+            db.session.delete(tournament)
+            db.session.commit()
+            
+            return {'message': 'Tournament deleted!'}, 200
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
     
+
 class Matches(Resource):
     def get(self):
         matches = Match.query.all()
         return [match.to_dict() for match in matches]
+
+    def post(self):
+        try:
+            data = request.get_json()
+            match = Match(team1=data['team1'], team2=data['team2'], date=data['date'], location=data['location'])
+            db.session.add(match)
+            db.session.commit()
+            return {'message': 'Match created!', 'match': match.to_dict()}, 201
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
+    def put(self, match_id):
+        try:
+            match = Match.query.get(match_id)
+            if not match:
+                return {'message': 'Match not found!'}, 404
+            
+            data = request.get_json()
+            match.team1 = data['team1']
+            match.team2 = data['team2']
+            match.date = data['date']
+            match.location = data['location']
+            db.session.commit()
+            
+            return {'message': 'Match updated!', 'match': match.to_dict()}, 200
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
+
+    def delete(self, match_id):
+        try:
+            match = Match.query.get(match_id)
+            if not match:
+                return {'message': 'Match not found!'}, 404
+            
+            db.session.delete(match)
+            db.session.commit()
+            
+            return {'message': 'Match deleted!'}, 200
+        except Exception as e:
+            return {'message': 'Something went wrong!', 'error': str(e)}, 400
 
 api.add_resource(Home, '/')
 api.add_resource(Signup, '/signup')
