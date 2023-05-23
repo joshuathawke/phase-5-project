1de7497a36dcf2fcc34e5ffdd80766ef9c7ff7dd
joshuathawke <joshuathawke@gmail.com>
2023-05-23 13:35:30 -0400

    landingpage update

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -1,49 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between bg-gray-900 p-4">
+      <div className="text-white">
+        <Link to="/">Esports Elevated</Link>
+      </div>
+      <div className="flex">
+        <Link to="/tournaments" className="text-white mx-4">
+          Tournaments
+        </Link>
+        <Link to="/teams" className="text-white mx-4">
+          Teams
+        </Link>
+        <Link to="/login" className="text-white mx-4">
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
 const LandingPage = () => {
   return (
     <div className="bg-black text-white">
+      <Navbar />
       <header className="text-center py-10">
         <h1 className="text-4xl font-bold">Esports Elevated</h1>
         <p className="mt-4">Elevate your Game!</p>
       </header>
       <section className="px-4 py-8 text-center">
-        <h2 className="text-2xl font-bold">Tournaments</h2>
-        <p className="mt-2">Upcoming tournaments:</p>
-        <ul className="list-disc list-inside mt-2">
-          <li>
-            <Link to="/tournaments" className="text-blue-500 hover:underline">
-              Tournament 1
-            </Link>
-          </li>
-          <li>
-            <Link to="/tournaments" className="text-blue-500 hover:underline">
-              Tournament 2
-            </Link>
-          </li>
-          <li>
-            <Link to="/tournaments" className="text-blue-500 hover:underline">
-              Tournament 3
-            </Link>
-          </li>
-        </ul>
+       
       </section>
       <section className="px-4 py-8 text-center">
         <h2 className="text-2xl font-bold">Join The Games!</h2>
-        <p className="mt-2">Sign up or log in to your account:</p>
         <div className="flex justify-center">
           <Link
             to="/signup"
             className="px-4 py-2 mr-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           >
             Sign Up
-          </Link>
-          <Link
-            to="/login"
-            className="px-4 py-2 ml-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Login
           </Link>
         </div>
       </section>
